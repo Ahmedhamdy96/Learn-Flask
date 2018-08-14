42093355f0b9fde0822803c7f4092ba4bcb2ba48
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2018-08-15 01:25:26 +0200

    enable debug mode and run as a normal python code

--- app_one/my_app.py
+++ app_one/my_app.py
@@ -12,3 +12,11 @@ app = Flask(__name__)                    # creating a Flask Object , __name__ is
 @app.route("/")                          # decorator to tell Flask what URL should trigger home() function
 def home():
     return " welcome to home page "
+
+
+
+
+#  [ 1 ] If you enable debug support the server will reload itself on code changes,and it will also provide you with a helpful debugger if things go wrong.
+#  [ 2 ] This if __name__=='__main__'      => To run the code with  [ $python3 my_app.py ] instead of [ $flask run ]
+if __name__=='__main__':
+    app.run(debug=True)
