bbdd642f0d0a33322af48a093cf1fcfa5268aa0c
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2018-08-15 02:12:47 +0200

    Variable Rules

--- app_one/my_app.py
+++ app_one/my_app.py
@@ -19,7 +19,20 @@ def home():
 def about():
     return "my name is ahmed from mansoura "
 
+# ===================================== Variable Rules =====================================================
+# < converter ( type ) : variable-name >
+@app.route("/users/<username>")                      # 127.0.0.1:5000/users/ahmed ... /osama ...etc
+def show_profile(username):
+    return "User Name :  {} ".format(username)
 
+
+@app.route("/users/<int:id>")
+def shoe_userId(id):
+    return "User ID  : {} ".format(id)
+
+@app.route("/home/<path:subpath>")
+def shoe_supPath(subpath):
+    return "Path is : {} ".format(subpath)
 
 #  [ 1 ] If you enable debug support the server will reload itself on code changes,and it will also provide you with a helpful debugger if things go wrong.
 #  [ 2 ] This if __name__=='__main__'      => To run the code with  [ $python3 my_app.py ] instead of [ $flask run ]
