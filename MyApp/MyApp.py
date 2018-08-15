3149076b94d778cb7ef28635bf0c8f135d224ce0
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2018-08-16 01:32:11 +0200

    using Templates and jinja template Designer Syntax

--- MyApp/MyApp.py
+++ MyApp/MyApp.py
@@ -2,9 +2,22 @@ from flask import Flask
 from flask import render_template   # to render html , css , js files
 app = Flask(__name__)
 #====================================================================================
+posts = [
+        {'title':'Python vs C ',
+         'name':'ahmed' ,
+         'date':'August 2018' ,
+         'post':'I Love both Python and C  '
+         } ,
+         {
+         'title' : 'Coffe vs Tea ' ,
+         'name':'Mohamed' ,
+         'date':'August 2018' ,
+         'post':'My name is mohamed and i love coffe more than tea'
+         }
+        ]
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html' , posts=posts) # we have access to that variable (posts) in the home.html template
 
 @app.route('/about')
 def about():
