cafe476f2ca4cb712ed12c18387c6e75cb83cea4
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2018-08-17 00:07:00 +0200

    add a route for Registration and Login Forms and Generating a secret key

--- MyApp/MyApp.py
+++ MyApp/MyApp.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask import render_template   # to render html , css , js files
+
+from os import urandom #  urandom(16) => to generate a secret key os 16 chars for example same as token_bytes(16) =>> urandom(16).hex()
+from forms import RegistrationForm , LoginForm
+
 app = Flask(__name__)
+# app.config['SECRET_KEY'] = '' # urandom(16).hex() => at Desktop / key 
 #====================================================================================
 posts = [
         {'title':'Python vs C ',
@@ -22,6 +27,17 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/regestration')
+def regestration():
+    form = RegistrationForm() # creating an instance of the RegistrationForm Class
+    return render_template('registration.html'  , title = 'Registration' , form = form )
+    # form = form make us able to accecc the RegistrationForm object in the html template
+
+@app.route('/login')
+def login():
+    form = LoginForm()  # creating an object of the login form and name it form
+    return render_template('login.html' , title = 'Login' , form = form) # we can access this object at the html by the name form
 #=====================================================================================
 if __name__=='__main__':
     app.run(debug=True)
